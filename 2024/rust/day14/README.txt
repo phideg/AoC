fd5391b89b184777c2cd837458d21074e65682d6
Philipp Degler <philipp.degler@sap.com>
2025-01-28 01:10:40 +0100

    solution for day14

--- 2024/rust/day14/README.txt
+++ 2024/rust/day14/README.txt
@@ -134,7 +134,7 @@ don't count as being in any quadrant, so the only relevant robots are:
 ..... 2..1.
 ..... .....
 1.... .....
-           
+
 ..... .....
 ...12 .....
 .1... 1....
@@ -146,4 +146,12 @@ Predict the motion of the robots in your list within a space which is 101 tiles
 wide and 103 tiles tall. What will the safety factor be after exactly 100
 seconds have elapsed?
 
-To begin, get your puzzle input.
+--- Part Two ---
+
+During the bathroom break, someone notices that these robots seem awfully
+similar to ones built and used at the North Pole. If they're the same type of
+robots, they should have a hard-coded Easter egg: very rarely, most of the
+robots should arrange themselves into a picture of a Christmas tree.
+
+What is the fewest number of seconds that must elapse for the robots to display
+the Easter egg?
--- 2024/rust/day14/src/main.rs
+++ 2024/rust/day14/src/main.rs
@@ -1,12 +1,12 @@
-use std::str::FromStr;
+use std::{cmp::Ordering, str::FromStr};
 
 #[derive(Debug)]
 struct RobotParseError;
 
 #[derive(Default, Debug, PartialEq)]
 struct Robot {
-    p_x: isize,
-    p_y: isize,
+    p_x: usize,
+    p_y: usize,
     v_x: isize,
     v_y: isize,
 }
@@ -53,56 +53,101 @@ impl FromStr for Robot {
     }
 }
 
-fn main() {
-    let input = include_str!("../puzzle_input");
-    println!("Part 1: {}", part1(input, 100, 101, 103));
-    // println!("Part 2: {}", part2(input));
+impl Robot {
+    fn update_pos(&mut self, seconds: usize, wide: usize, tall: usize) {
+        let r = self;
+        r.p_x = match r.v_x.cmp(&0) {
+            Ordering::Less => (r.p_x + (seconds * (wide - r.v_x.unsigned_abs()))) % wide,
+            Ordering::Greater => ((seconds * r.v_x as usize) + r.p_x) % wide,
+            Ordering::Equal => r.p_x,
+        };
+        r.p_y = match r.v_y.cmp(&0) {
+            Ordering::Less => (r.p_y + (seconds * (tall - r.v_y.unsigned_abs()))) % tall,
+            Ordering::Greater => (r.p_y + (seconds * r.v_y as usize)) % tall,
+            Ordering::Equal => r.p_y,
+        };
+    }
 }
 
-// fn eval_robots(robots: &[Robot]) -> usize {
-//     let quadrants = robots
-//         .iter()
-//         .filter(|v| v.p_x != middle_x && v.p_y != middle_y)
-//         .fold([0_usize, 0, 0, 0], |mut acc, v| {
-//             match (v.p_x < middle_x, v.p_y < middle_y) {
-//                 (true, true) => acc[0] += 1,
-//                 (true, false) => acc[1] += 1,
-//                 (false, true) => acc[2] += 1,
-//                 (false, false) => acc[3] += 1,
-//             }
-//             acc
-//         });
-//     quadrants.iter().filter(|&v| *v != 0).product()
-// }
+fn print_robots(robots: &[Robot], width: usize, height: usize) {
+    let mut field = vec![vec![0_usize; width]; height];
+    robots.iter().for_each(|r| field[r.p_y][r.p_x] += 1);
+    field.iter().for_each(|line| {
+        line.iter().for_each(|p| {
+            if p == &0 {
+                print!(".");
+            } else {
+                print!("{p}");
+            }
+        });
+        println!();
+    });
+}
 
-fn part1(input: &str, seconds: isize, wide: isize, tall: isize) -> usize {
-    let robots = input
+fn get_quadrants(robots: &[Robot], width: usize, height: usize) -> [usize; 4] {
+    // sum quadrants
+    let middle_x = width / 2;
+    let middle_y = height / 2;
+    robots.iter().fold([0_usize, 0, 0, 0], |mut acc, r| {
+        match (r.p_x.cmp(&middle_x), r.p_y.cmp(&middle_y)) {
+            (Ordering::Less, Ordering::Less) => acc[0] += 1,
+            (Ordering::Greater, Ordering::Less) => acc[1] += 1,
+            (Ordering::Less, Ordering::Greater) => acc[2] += 1,
+            (Ordering::Greater, Ordering::Greater) => acc[3] += 1,
+            _ => {}
+        }
+        acc
+    })
+}
+
+fn get_robots(input: &str, seconds: usize, width: usize, height: usize) -> Vec<Robot> {
+    input
         .lines()
         .map(|line| {
             line.parse::<Robot>()
                 .map(|mut r| {
-                    r.p_x = if r.v_x < 0 {
-                        (r.p_x + (seconds * (wide + r.v_x))) % wide
-                    } else if r.v_x > 0 {
-                        ((seconds * r.v_x) + r.p_x) % wide
-                    } else {
-                        r.p_x
-                    };
-                    r.p_y = if r.v_y < 0 {
-                        (r.p_y + (seconds * (tall + r.v_y))) % tall
-                    } else if r.v_x > 0 {
-                        (r.p_y + (seconds * r.v_y)) % tall
-                    } else {
-                        r.p_y
-                    };
+                    r.update_pos(seconds, width, height);
                     r
                 })
                 .unwrap()
         })
-        .collect::<Vec<_>>();
-    let middle_x = (wide / 2) + 1;
-    let middle_y = (tall / 2) + 1;
-    todo!()
+        .collect::<Vec<_>>()
+}
+
+fn part1(input: &str, seconds: usize, width: usize, height: usize) -> usize {
+    let robots = get_robots(input, seconds, width, height);
+    print_robots(&robots, width, height);
+    let quadrants = get_quadrants(&robots, width, height);
+    quadrants.iter().product()
+}
+
+fn part2(input: &str, width: usize, height: usize) -> usize {
+    println!("{input}, {width}, {height}");
+    //// while watching the output of a loop it seemed obvious
+    //// that there is pattern reoccurring every 101 seconds
+    //// starting from 77 onwards
+    // let mut i = 77;
+    // loop {
+    //     let robots = get_robots(input, i, width, height);
+    //     print_robots(&robots, width, height);
+    //     println!("{i}");
+    //     sleep(time::Duration::from_millis(100));
+    //     i += 101;
+    // };
+    // at following point in time a tree was visible
+    7753
+}
+
+fn main() {
+    let width = 101;
+    let height = 103;
+    let input = include_str!("../puzzle_input");
+    println!("Part 1: {}", part1(input, 100, width, height));
+    let part2 = part2(input, width, height);
+    println!("Part 2: {part2}");
+    // print the easteregg
+    let robots = get_robots(input, part2, width, height);
+    print_robots(&robots, width, height);
 }
 
 #[cfg(test)]
@@ -117,6 +162,21 @@ mod tests {
                 p_x: 0,
                 p_y: 4,
                 v_x: 3,
+                v_y: -3
+            }
+        );
+    }
+
+    #[test]
+    fn test_update_pos() {
+        let mut robot = "p=2,4 v=2,-3".parse::<Robot>().unwrap();
+        robot.update_pos(5, 11, 7);
+        assert_eq!(
+            robot,
+            Robot {
+                p_x: 1,
+                p_y: 3,
+                v_x: 2,
                 v_y: -3
             }
         );
